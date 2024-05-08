4004b1e1f1a351ed27c695cca014176df8eb44e2
athulantony95 <154115716+athulantony95@users.noreply.github.com>
2024-05-08 11:39:29 +0530

    iOS build support

--- ios/IPFSModule.m
+++ ios/IPFSModule.m
@@ -7,29 +7,55 @@
 
 #import "IPFSModule.h"
 #import <React/RCTLog.h>
+#import "RNGoMobileIPFS-Swift.h"
 
 @implementation IPFSModule
+{
+  IPFSInterOp *ipfs;
+}
 
 // To export a module named IPFSModule
 RCT_EXPORT_MODULE(IPFSModule);
 
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    ipfs = [[IPFSInterOp alloc] init];
+  }
+  return self;
+}
+
 RCT_EXPORT_METHOD(sendMessage:(NSString *)message)
 {
   RCTLogInfo(@"Sending message %@", message);
+  [ipfs sendMessageWithMessage:message];
 }
-
 
 
 RCT_EXPORT_METHOD(startSubscription)
 {
   RCTLogInfo(@"Starting subscription");
+  [ipfs startSubscriptionWithCallback:^(NSString * _Nullable message) {
+    NSLog(@"%@", message);
+    [self sendEventWithName:@"ORBITDB" body:@{@"message": message}];
+  }];
+  
+  [ipfs startPeerCounterWithCallback:^(NSString * _Nullable message) {
+    [self sendEventWithName:@"PEERS" body:@{@"peers": message}];
+  }];
+  
 }
 
 RCT_EXPORT_METHOD(start:(RCTResponseSenderBlock)callback)
 {
-  NSString *eventId = @"1234566";
-  
-  callback(@[eventId]);
+  NSString *result = [ipfs start];
+  callback(@[result]);
+}
+
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"ORBITDB", @"PEERS"];
 }
 
 @end
---
+++ ios/RNGoMobileIPFS-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  RNGoMobileIPFS-Bridging-Header.h
+//  RNGoMobileIPFS
+//
+//  Created by APPLE on 08/05/24.
+//
+
+#ifndef RNGoMobileIPFS_Bridging_Header_h
+#define RNGoMobileIPFS_Bridging_Header_h
+
+#endif /* RNGoMobileIPFS_Bridging_Header_h */
