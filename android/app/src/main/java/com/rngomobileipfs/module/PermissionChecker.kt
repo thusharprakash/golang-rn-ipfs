e7701a670e32528ee85d7f6b1f32a19b89dc9b26
athulantony95 <154115716+athulantony95@users.noreply.github.com>
2024-05-31 07:06:19 +0530

    Fix old android version check

--- android/app/src/main/java/com/rngomobileipfs/module/PermissionChecker.kt
+++ android/app/src/main/java/com/rngomobileipfs/module/PermissionChecker.kt
@@ -10,17 +10,28 @@ import androidx.core.content.ContextCompat
 
 object PermissionChecker {
 
-    private val permissions = arrayOf(
-        Manifest.permission.BLUETOOTH_ADVERTISE,
-        Manifest.permission.BLUETOOTH_CONNECT,
-        Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.INTERNET,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.CHANGE_WIFI_MULTICAST_STATE
-    )
+    private val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CHANGE_WIFI_MULTICAST_STATE
+        )
+    } else {
+       arrayOf(
+           Manifest.permission.BLUETOOTH,
+           Manifest.permission.BLUETOOTH_ADMIN,
+           Manifest.permission.INTERNET,
+           Manifest.permission.ACCESS_FINE_LOCATION,
+           Manifest.permission.ACCESS_COARSE_LOCATION,
+           Manifest.permission.CHANGE_WIFI_MULTICAST_STATE
+       )
+    }
 
     val REQUEST_PERMISSION_CODE = 1001
 
